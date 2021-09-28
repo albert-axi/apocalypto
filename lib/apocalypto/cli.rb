ba194204532e92b2b7a4b21892450910003e8b64
jmhar <jmhar@protonmail.com>
2021-09-28 19:57:02 +1000

    set up basic file structure

---
+++ lib/apocalypto/cli.rb
@@ -0,0 +1,21 @@
+class Apocalypto::CLI
+    def initialize
+        Apocalypto::Scraper.new.get_locations
+    end
+
+    def start
+        system("clear")
+        puts "Welcome to Apocalypto"
+        puts "══════════ ∘◦ ❈ ◦∘ ══════════"
+        puts "It's the end of days. A plague has taken over the world, turning people into vicious, flesh eating zombies. The world as you know it is over. You job now? Survive."
+
+    end
+
+    def break
+        puts ""
+    end
+end
+zombie = ‿︵‿︵(ಥ﹏ಥ)‿︵‿︵
+loading = ●・○・●
+hit = (ﾉ>｡☆)ﾉ
+oh_no = ᵒʰ(⑉・̆⌓・̆⑉)ɴᴏ
---
+++ lib/apocalypto_app.rb
@@ -0,0 +1,9 @@
+require 'open-uri'
+require 'nokogiri'
+require_relative './apocalypto/supplies.rb'
+require_relative './apocalypto/scraper.rb'
+require_relative './apocalypto/cli.rb'
+
+module Apocalypto
+
+end
